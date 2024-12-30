b6e9e0b401a0d4970b951e44ceb5b11cd16feca1
Saad Sayyed <saadsyed950@gmail.com>
2024-12-30 14:49:00 +0530

    Added function of addAdress

---
+++ controller/addressController.js
@@ -0,0 +1,12 @@
+const Address = require("../model/Address");
+
+const addAddress = async (req, res) => {
+  try {
+    const { title, houseNumber, area, location } = req.body;
+    const newAddress = new Address({ title, houseNumber, area, location });
+    await newAddress.save();
+    res.status(201).json({ success: true, data: newAddress });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
