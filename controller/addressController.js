d71507be52016122521b529bc0fc761cd98ee9d9
Saad Sayyed <saadsyed950@gmail.com>
2024-12-30 18:54:36 +0530

    Added route and controller for updating address

--- controller/addressController.js
+++ controller/addressController.js
@@ -35,4 +35,22 @@ const getSingleAddress = async (req, res) => {
   }
 };
 
-module.exports = { addAddress, getAllAddress, getSingleAddress };
+const updateAddress = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, houseNumber, area, location } = req.body;
+    const address = { title, houseNumber, area, location };
+
+    const updatedAddress = await Address.findByIdAndUpdate(id, address);
+
+    if (!updatedAddress) {
+      res.status(404).json({ message: "Address not found" });
+    }
+
+    res.status(201).json({ success: true, data: updatedAddress });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = { addAddress, getAllAddress, getSingleAddress, updateAddress };
--- routes/addressRouter.js
+++ routes/addressRouter.js
@@ -3,11 +3,13 @@ const {
   addAddress,
   getAllAddress,
   getSingleAddress,
+  updateAddress,
 } = require("../controller/addressController");
 const router = express.Router();
 
 router.post("/", addAddress);
 router.get("/", getAllAddress);
 router.get("/:id", getSingleAddress);
+router.put("/:id", updateAddress);
 
 module.exports = router;
